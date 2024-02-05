abdd288a8300972b25d5af16f1e5ab9f402493be
aa1555 <144973990+aa1555@users.noreply.github.com>
2024-02-06 07:56:39 +0800

    Create 爬取网页图片.py

---
+++ Misc/Python/爬取网页图片.py
@@ -0,0 +1,33 @@
+import requests
+from lxml import etree
+
+# 设置请求头
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36'
+}
+
+# 设置目标网页的URL
+url = 'https://www.mn52.com/xingganmeinv/1053689.html'
+
+# 发送GET请求
+response = requests.get(url, headers=headers)
+
+# 解析HTML响应
+html = etree.HTML(response.text)
+
+# 使用XPath获取图片URL
+image_urls = html.xpath('//div[@id="originalpic"]/img/@src')
+
+# 遍历图片URL列表，下载图片
+for image_url in image_urls:
+    # 拼接完整的图片URL
+    full_image_url = 'https://www.mn52.com' + image_url
+    # 发送GET请求下载图片
+    image_response = requests.get(full_image_url, headers=headers)
+    
+    # 提取图片文件名
+    image_filename = image_url.split('/')[-1]
+    
+    # 保存图片到本地
+    with open(image_filename, 'wb') as f:
+        f.write(image_response.content)
