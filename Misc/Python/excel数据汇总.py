678f9eb86d47bfe6a399464306ac777810b29f53
aa1555 <144973990+aa1555@users.noreply.github.com>
2024-02-06 08:14:31 +0800

    更新 excel数据汇总.py

--- Misc/Python/excel数据汇总.py
+++ Misc/Python/excel数据汇总.py
@@ -1,4 +1,3 @@
-
 import os
 from openpyxl import load_workbook
 
@@ -50,3 +49,14 @@ try:
     summary_workbook.save('d:/files/temp/汇总23.xlsx')
 except Exception as e:
     print(f"Error saving summary workbook: {e}")
+
+'''
+这段代码实现的功能是将一个目录下所有以.xlsx结尾的Excel文件中的特定数据提取出来，并将这些数据汇总到一个新的Excel文件中。具体来说，它会执行以下步骤：
+
+加载一个已经存在的Excel文件作为汇总工作簿，这个文件将被用来存储从其他Excel文件中提取的数据。
+遍历指定目录下的所有文件。
+对于每个以.xlsx结尾的文件，假设它是一个Excel文件，代码会打开这个文件并读取默认工作表中的特定单元格数据。这些特定单元格是B2、C3、C4和F4。
+将从每个文件中读取的数据追加到汇总工作簿的末尾。
+在处理完所有文件后，将汇总工作簿保存到指定的路径。
+最终，你会得到一个包含所有指定Excel文件特定数据的汇总Excel文件。这个过程可以用来整合多个Excel文件的数据，以便于分析和报告。
+'''
