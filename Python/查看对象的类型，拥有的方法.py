b6544bd5973d38ca9eb6522ff31d29e1da19e5b0
aa1555 <144973990+aa1555@users.noreply.github.com>
2024-07-31 19:00:31 +0800

    更新 查看对象的类型，拥有的方法.py

--- Python/查看对象的类型，拥有的方法.py
+++ Python/查看对象的类型，拥有的方法.py
@@ -1,7 +1,7 @@
 
 c=
-print("打印该对象:\n",c,"\n")
-print("该对象的类型:\n",type(c),"\n")
-print("该对象拥有的方法：")
+print("打印该对象:\n",c,"\n\n")
+print("该对象的类型:\n",type(c),"\n\n")
+print("该对象的方法：")
 for i in dir(c):
     print(i)
